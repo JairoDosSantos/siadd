98d3543cbedf9ba6c19b8cd650f4b43d29276126
Jairo dos Santos <jairocrissantos@gmail.com>
2023-11-19 17:06:43 +0100

    Update api.ts
    change my api url.

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: "http://192.168.20.153:8000/api" });
+const api = axios.create({ baseURL: "https://instic.uniluanda.ao/siadd_back/api" });
 //const api = axios.create({ baseURL: "https://3521-41-205-53-233.ngrok-free.app/api" });
 
 export { api };
